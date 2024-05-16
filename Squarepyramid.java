12a1899b9abb4e99439ee97e7dc9f40586ca2353
jeetxgeek <mauryajeetesh57@gmail.com>
2024-05-16 22:54:50 +0530

    Squarepyramid program added

---
+++ Squarepyramid.java
@@ -0,0 +1,24 @@
+package Localrepo;
+
+import java.util.Scanner;
+
+/**
+ * Innerprogram3
+ */
+public class Squarepyramid {
+    public static void main(String[] args) {
+        Scanner l = new Scanner(System.in);
+        int n;int i; int j;
+        System.out.println("enter the number for sequence:");
+        n = l.nextInt();
+        for(i=0;i<n;i++){
+            for(j=0;j<n;j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+
+
+    }
+    
+}
