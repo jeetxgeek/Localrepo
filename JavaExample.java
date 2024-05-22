e284be6a8ee478ed678754b18f8cff6c7bd8979a
jeetxgeek <mauryajeetesh57@gmail.com>
2024-05-22 18:22:41 +0530

    factorial modified

--- JavaExample.java
+++ JavaExample.java
@@ -19,7 +19,7 @@ public static void main(String[] args) {
           
         Scanner sc = new Scanner(System.in);  
   
-        //accepting a number from the user  
+        //accepting a number from the user
         System.out.println("Enter a number whose factorial is to be found: ");  
 int num = sc.nextInt();  
           
